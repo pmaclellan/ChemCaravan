7634c0026a96705f7014cc2b20f5b4f465893eb5
Pete MacLellan <maclellan.pete@gmail.com>
2016-05-23 15:58:37 -0700

    basic home page and login renaming

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,21 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController} from 'ionic-angular';
+import {LocalLoginPage} from '../local-login/local-login';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
+  private nav: NavController = null;
 
+  constructor(nav:NavController) {
+    this.nav = nav;
+  }
+
+  goToLocalLogin() {
+    this.nav.push(LocalLoginPage);
+  }
+
+  goToOnlineLogin() {
+    alert("Online version not implemented yet!");
   }
 }
