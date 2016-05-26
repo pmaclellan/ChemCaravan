22b9653c1b45a02dfdb3293c9717a1aaf9afab8d
pmaclellan <maclellan.pete@gmail.com>
2016-05-25 21:20:24 -0700

    adds ability to create new player

--- app/pages/local-login/local-login.ts
+++ app/pages/local-login/local-login.ts
@@ -8,19 +8,31 @@ export class LocalLoginPage {
   private nav: NavController;
   private sqlService: SqlService;
   private player: any;
+  private showSignup: Boolean;
 
   constructor(nav: NavController, sqlService: SqlService) {
     this.nav = nav;
     this.sqlService = new SqlService();
     this.player = this.sqlService.loadPlayerState();
+    this.showSignup = false;
   }
 
   continue() {
     console.log('continue with player ' + this.player.name);
+    this.sqlService.loadPlayerState();
   }
 
   startNewGame() {
+    //TODO: a warning popup would be nice here
     this.sqlService.clearPlayerState();
     console.log('start a new game')
+    this.showSignup = true;
+  }
+
+  createPlayer(name: string) {
+    this.player = new Player(name);
+    console.log('player created: ' + name);
+    this.sqlService.savePlayerState(this.player);
+    console.log('player saved');
   }
 }
