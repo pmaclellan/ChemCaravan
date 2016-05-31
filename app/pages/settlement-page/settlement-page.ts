225f0f646373b2392e5f9b3870394c24d634a765
Pete MacLellan <maclellan.pete@gmail.com>
2016-05-31 14:15:50 -0700

    attempt at side menu, not quite there yet

--- app/pages/settlement-page/settlement-page.ts
+++ app/pages/settlement-page/settlement-page.ts
@@ -1,4 +1,4 @@
-import {Page, NavController, NavParams} from 'ionic-angular';
+import {Page, NavController, NavParams, MenuController} from 'ionic-angular';
 import {Player} from '../../providers/classes/player';
 import {Settlement} from '../../providers/classes/settlement';
 import {SettlementService} from '../../providers/services/settlement-service';
@@ -12,15 +12,27 @@ export class SettlementPage {
 	private nav: NavController;
 	private navParams: NavParams;
 	private sqlService: SqlService;
+  private settlementService: SettlementService;
 	private player: Player;
 	private settlement: Settlement;
+  private settlements: Settlement[];
+  private menu: MenuController;
 
-	constructor(nav: NavController, navParams: NavParams, sqlService: SqlService) {
+	constructor(nav: NavController, navParams: NavParams, 
+							sqlService: SqlService, settlementService: SettlementService,
+							menu: MenuController) {
 		this.nav = nav;
 		this.navParams = navParams;
 		this.sqlService = sqlService;
+    this.settlementService = settlementService;
+    this.menu = menu;
 
 		this.player = navParams.get('player');
 		this.settlement = navParams.get('settlement');
+    this.settlements = settlementService.getSettlements();
+    //don't allow navigation to current settlement
+    this.settlements.filter((value: Settlement) => value != this.settlement);
+    console.log(this.menu.getMenus());
+    this.menu.open();
 	}
 }
