eabe4e11bb68687dd8e1f7b4363543b14954e895
Pete MacLellan <maclellan.pete@gmail.com>
2016-05-31 17:17:30 -0700

    added ChemService provider

--- app/providers/classes/chem.ts
+++ app/providers/classes/chem.ts
@@ -1,5 +1,13 @@
 export class Chem {
   name: string;
   basePrice: number;
-  stdDev: number;
+  currentPrice: number;
+  probability: number;
+
+  constructor(name: string, basePrice: number, probability: number) {
+    this.name = name;
+    this.basePrice = basePrice;
+    this.currentPrice = basePrice;
+    this.probability = probability;
+  }
 }
