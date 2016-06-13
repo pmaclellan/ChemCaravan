0a16076708a170371f0a02c3088b8573ac5a816b
Pete MacLellan <maclellan.pete@gmail.com>
2016-06-13 12:52:35 -0700

    overhaul sell page to use slider and share profits with guards

--- app/pages/sell-chem/sell-chem.ts
+++ app/pages/sell-chem/sell-chem.ts
@@ -1,14 +1,12 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
-import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators, AbstractControl} from '@angular/common';
 import {Player} from '../../providers/classes/player';
 import {Chem} from '../../providers/classes/chem';
 import {SqlService} from '../../providers/services/sql-storage-service';
 
 @Component({
   templateUrl: 'build/pages/sell-chem/sell-chem.html',
-  providers: [SqlService],
-  directives: [FORM_DIRECTIVES]
+  providers: [SqlService]
 })
 export class SellChemPage {
   private nav: NavController;
@@ -16,12 +14,10 @@ export class SellChemPage {
   private sqlService: SqlService;
   private player: Player;
   private chem: Chem;
-  private sellForm: ControlGroup;
   private maxSellable: number;
-  private quantity: AbstractControl;
+  private quantity: number;
 
-  constructor(nav: NavController, navParams: NavParams,
-    sqlService: SqlService, fb: FormBuilder) {
+  constructor(nav: NavController, navParams: NavParams, sqlService: SqlService) {
     this.nav = nav;
     this.navParams = navParams;
     this.sqlService = sqlService;
@@ -29,25 +25,13 @@ export class SellChemPage {
     this.player = navParams.get('player');
     this.chem = navParams.get('chem');
     this.maxSellable = this.player.quantityCarrying(this.chem);
-
-    this.sellForm = fb.group({
-      'quantity': ['', Validators.compose([Validators.required])]
-    })
-    this.quantity = this.sellForm.controls['quantity'];
+    this.quantity = this.maxSellable;
   }
 
-  onSubmit(value: number): void {
-    if (this.sellForm.valid) {
-      //do some bounds checking, this will be removed once Ionic 2 adds range sliders
-      if (this.quantity.value < 0 || this.quantity.value > this.maxSellable) {
-        alert("check your bounds and try again");
-        return;
-      }
-      console.log('Submitted value: ', value);
-      this.player.sell(this.chem, this.quantity.value);
-      this.sqlService.savePlayerState(this.player);
-      this.nav.pop();
-    }
+  sell(): void {
+    this.player.sell(this.chem, this.quantity, this.getGuardsShare());
+    this.sqlService.savePlayerState(this.player);
+    this.nav.pop();
   }
 
   getProfitMargin(): number {
@@ -57,6 +41,19 @@ export class SellChemPage {
                            this.player.pricePaid(this.chem)) * 10000 ) / 100;
     } else {
       return null;
+    }
+  }
+
+  getProfit(): number {
+    return this.getProfitMargin() / 100 * this.player.pricePaid(this.chem) * this.quantity;
+  }
+
+  getGuardsShare(): number {
+    let profit: number = this.getProfit();
+    if (profit <= 0) {
+      return 0;
+    } else {
+      return Math.floor(profit * this.player.guards * 0.05);
     }
   }
 }
